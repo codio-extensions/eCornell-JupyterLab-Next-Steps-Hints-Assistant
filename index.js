ca9d82232140205e5e39c2969371b55e063e3325
Mohit Chandarana <mchandarana@codio.com>
2025-06-02 06:09:59 +0000

    Updated the repo with updates for this assistant - and added README

--- index.js
+++ index.js
@@ -1,43 +1,55 @@
 // Wrapping the whole extension in a JS function 
 // (ensures all global variables set in this extension cannot be referenced outside its scope)
 (async function(codioIDE, window) {
-  
-  // Refer to Anthropic's guide on system prompts here: https://docs.anthropic.com/claude/docs/system-prompts
-  const systemPrompt = "System Prompt for the LLM goes here"
-  
+
   // register(id: unique button id, name: name of button visible in Coach, function: function to call when button is clicked) 
-  codioIDE.coachBot.register("iNeedHelpButton", "I have a question", onButtonPress)
+  codioIDE.coachBot.register("customHintsJupyter", "Provide a hint on what to do next", onButtonPress)
 
   // function called when I have a question button is pressed
   async function onButtonPress() {
 
-    // Function that automatically collects all available context 
-    // returns the following object: {guidesPage, assignmentData, files, error}
-    const context = await codioIDE.coachBot.getContext()
-
-    // the messages object that will contain the user prompt and/or any assistant responses to be sent to the LLM
-    // Refer to Anthropic's guide on the messages API here: https://docs.anthropic.com/en/api/messages
-    let messages = []
-
-    const userPrompt = "User prompt for the LLM goes here - can contain context from the object returned by getContext()"
-
-    // Add user prompt to messages object
-    messages.push({
-        "role": "user", 
-        "content": userPrompt
-    })
-
-    // Send the API request to the LLM with all prompts and context 
-    const result = await codioIDE.coachBot.ask({
-      systemPrompt: systemPrompt,
-      messages: messages
-    })
+    // automatically collects all available context 
+    let context = await codioIDE.coachBot.getContext()
+    // console.log(context)
     
+    // select open jupyterlab notebook related context
+    let openJupyterFileContext = context.jupyterContext[0]
+    let jupyterFileName = openJupyterFileContext.path
+    let jupyterFileContent = openJupyterFileContext.content
+    
+    // filter and map cell indices of code and markdown cells into a new array
+    const markdownAndCodeCells = jupyterFileContent.map(
+        ({ id, ...rest }, index) => ({
+              cell: index,
+            ...rest
+        })).filter(
+            obj => obj.type === 'markdown' || obj.type === 'code'
+        )
+    // console.log("code and markdown", JSON.stringify(markdownAndCodeCells))
+
+    const systemPrompt = `You are an assistant helping students understand and make progress themselves on their programming assignments. 
+You will be provided with the jupyter notebook they're working in.
+Based on this information, provide at most 2 relevant hints or ideas for things they can try next to make progress.
+Do not provide the full solution. 
+Do not ask if they have any other questions.
+  `
+        
+    const userPrompt = `Here is the student's jupyter notebook:
+
+<code>
+${JSON.stringify(markdownAndCodeCells)}
+</code> 
+
+If <code> is empty, assume that it's not available. 
+
+Phrase your hints directly addressing the student as 'you'.
+Phrase your hints as questions or suggestions.
+`
+
+    const result = await codioIDE.coachBot.ask({
+        systemPrompt: systemPrompt,
+        messages: [{"role": "user", "content": userPrompt}]
+    })
   }
-// calling the function immediately by passing the required variables
+
 })(window.codioIDE, window)
-
- 
-
-  
-  
